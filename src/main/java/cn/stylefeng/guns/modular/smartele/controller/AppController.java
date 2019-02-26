b11a0255eef0bf0d3cd4065c817bcad25488cd14
vector <vector.wang@hirede.com>
2019-02-26 18:31:19 +0800

    熟悉

--- src/main/java/cn/stylefeng/guns/modular/smartele/controller/AppController.java
+++ src/main/java/cn/stylefeng/guns/modular/smartele/controller/AppController.java
@@ -1,6 +1,7 @@
 package cn.stylefeng.guns.modular.smartele.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "移动端接口")
 public class AppController {
 
+    @ApiOperation(value = "接口测试", notes = "接口测试note")
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String test() {
         return "这是测试";
